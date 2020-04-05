b4d34d6c3b48340c849b1289f2df7e821ae2146f
Gilgamesh Nootebos <gilgamez@gmail.com>
2020-04-05 13:16:43 +0200

    feat(network_status): initial commit

---
+++ network_status.5s.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# <bitbar.title>Network Status</bitbar.title>
+# <bitbar.version>v1.0</bitbar.version>
+# <bitbar.author>Gilgamesh Nootebos</bitbar.author>
+# <bitbar.author.github>gilgamez</bitbar.author.github>
+# <bitbar.desc>Basic status indicator for internet connectivity<bitbar.desc>
+# <bitbar.image>http://i.imgur.com/Vulc3pu.png</bitbar.image>
+# <bitbar.dependencies>ping</bitbar.dependencies>
+# <bitbar.abouturl>http://example.com</bitbar.abouturl>
+
+echo hello
